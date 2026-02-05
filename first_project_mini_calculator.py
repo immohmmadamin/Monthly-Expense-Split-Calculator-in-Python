1edf3bdb54eee9efbcf3af47c0f1bf549b941186
immohmmadamin <immohmmad.amin@gmail.com>
2026-02-05 03:39:32 -0800

    Add files via upload
    This Python script is a simple expense-sharing calculator designed to divide monthly household expenses equally among a given number of people. It takes user input for common living costs such as rent, food expenses, and electricity bills, then calculates how much each person needs to pay.
    
    The program works through the command line and uses basic Python concepts, making it perfect for beginners who are learning:
    
    User input with input()
    
    Type conversion using int()
    
    Arithmetic operations
    
    Simple variable usage
    
    Output formatting with print()

---
+++ first_project_mini_calculator.py
@@ -0,0 +1,6 @@
+rent = int(input("Enter the rent = "))
+food = int(input("Enter the food bill = "))
+electricbill = int(input("Enter the electric city bill ="))
+person = int(input("Enter the person number : "))
+total_person_payment = (rent + food + electricbill)/person
+print(total_person_payment)
